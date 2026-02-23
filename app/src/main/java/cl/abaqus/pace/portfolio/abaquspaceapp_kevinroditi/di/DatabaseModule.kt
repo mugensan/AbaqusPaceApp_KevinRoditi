19a0d586fe11da751feed10003ad4523c5032850
mugensan <kev.roditi@gmail.com>
2026-02-23 13:07:18 -0300

    DI modules added

--- app/src/main/java/cl/abaqus/pace/portfolio/abaquspaceapp_kevinroditi/di/DatabaseModule.kt
+++ app/src/main/java/cl/abaqus/pace/portfolio/abaquspaceapp_kevinroditi/di/DatabaseModule.kt
@@ -1,4 +1,40 @@
 package cl.abaqus.pace.portfolio.abaquspaceapp_kevinroditi.di
 
-class DatabaseModule {
-}
+import cl.abaqus.pace.portfolio.abaquspaceapp_kevinroditi.data.local.dao.PositionDao
+import cl.abaqus.pace.portfolio.abaquspaceapp_kevinroditi.data.local.db.PortfolioDatabase
+import cl.abaqus.pace.portfolio.abaquspaceapp_kevinroditi.domain.repository.PortfolioRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+import kotlin.jvm.java
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule (
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext context:Context
+    ): AppDatabase =
+        Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "pace_database"
+    )
+        .fallbackToDestructiveMigration()
+        .build()
+
+    @Provides
+    fun providePortfolioDao(
+        database: PortfolioDatabase
+    ): PositionDao = database.positionDao()
+
+    @Provides
+    fun providePositionDao(
+        database: PortfolioDatabase
+    ): PositionDao = database.positionDao()
+)
+
--- app/src/main/java/cl/abaqus/pace/portfolio/abaquspaceapp_kevinroditi/di/RepositoryModule.kt
+++ app/src/main/java/cl/abaqus/pace/portfolio/abaquspaceapp_kevinroditi/di/RepositoryModule.kt
@@ -1,4 +1,21 @@
 package cl.abaqus.pace.portfolio.abaquspaceapp_kevinroditi.di
 
-class RepositoryModule {
+import cl.abaqus.pace.portfolio.abaquspaceapp_kevinroditi.data.local.repository.PortfolioRepositoryImpl
+import cl.abaqus.pace.portfolio.abaquspaceapp_kevinroditi.domain.repository.PortfolioRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule{
+
+    @Binds
+    @Singleton
+    abstract fun bindPortfolioRepository(
+        portfolioRepositoryImpl: PortfolioRepositoryImpl
+    ): PortfolioRepository
+
 }
